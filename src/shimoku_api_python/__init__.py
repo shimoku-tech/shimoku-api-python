8ac5ba4e6107d3ae92671a12d449b50786af694c
capitan-ariete <sergi.ortiz.rodriguez@gmail.com>
2022-07-20 11:13:22 +0200

    Some fixes in the __init__ after remove unused classes

--- src/shimoku_api_python/__init__.py
+++ src/shimoku_api_python/__init__.py
@@ -5,11 +5,9 @@ from shimoku_api_python.api.universe_metadata_api import UniverseMetadataApi
 from shimoku_api_python.api.business_metadata_api import BusinessMetadataApi
 from shimoku_api_python.api.app_metadata_api import AppMetadataApi
 from shimoku_api_python.api.app_type_metadata_api import AppTypeMetadataApi
-from shimoku_api_python.api.path_metadata_api import PathMetadataApi
 from shimoku_api_python.api.report_metadata_api import ReportMetadataApi
 from shimoku_api_python.api.data_managing_api import DataManagingApi
 from shimoku_api_python.api.plot_api import PlotApi
-from shimoku_api_python.api.suite_api import SuiteApi
 from shimoku_api_python.api.ping_api import PingApi
 
 from shimoku_api_python.client import ApiClient
@@ -30,12 +28,9 @@ class Client(object):
         self.business = BusinessMetadataApi(self._api_client)
         self.app_type = AppTypeMetadataApi(self._api_client)
         self.app = AppMetadataApi(self._api_client)
-        self.path = PathMetadataApi(self._api_client)
         self.report = ReportMetadataApi(self._api_client)
         self.data = DataManagingApi(self._api_client)
         self.plt = PlotApi(self._api_client)
-
-        self.suite = SuiteApi(self)  # it takes all the others classes
 
     def set_config(self, config={}):
         self.api_client.set_config(config)

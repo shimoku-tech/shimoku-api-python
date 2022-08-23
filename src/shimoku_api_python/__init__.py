64076dbc83202b7e61a0c8cf34a2976218e8cf9c
edgar <edgar.sarria@shimoku.com>
2022-08-23 09:11:25 +0200

    Clean initialization function for Client and clear_business() function in plot_api.py

--- src/shimoku_api_python/__init__.py
+++ src/shimoku_api_python/__init__.py
@@ -17,7 +17,12 @@ from shimoku_api_python.client import ApiClient
 
 
 class Client(object):
-    def __init__(self, universe_id: str, environment: str = 'staging', config={}):
+    def __init__(self, universe_id: str, environment: str = 'staging',
+                 acces_token: str = "", config={}, business_id: str = ""):
+
+        if acces_token != "":
+            config = {'access_token': acces_token}
+
         self._api_client = ApiClient(
             config=config,
             universe_id=universe_id,
@@ -36,5 +41,5 @@ class Client(object):
         self.plt = PlotApi(self._api_client)
         self.ai = AiAPI(self._api_client)
 
-    def set_config(self, config={}):
-        self.api_client.set_config(config)
+        if business_id != "":
+            self.plt.set_business(business_id)

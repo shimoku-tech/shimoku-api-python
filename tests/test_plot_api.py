fb8b85060737cf32a9a60be30dcb3f005432647d
edgar <edgar.sarria@shimoku.com>
2023-01-12 12:02:00 +0100

    Added audio recorder to input form tests

--- tests/test_plot_api.py
+++ tests/test_plot_api.py
@@ -3478,6 +3478,11 @@ def test_input_form():
                 'fieldName': 'Objetivos',
                 'inputType': 'multiSelect',
                 'options': ['sleep', 'close eyes', 'awake']
+              },
+              {
+                'mapping': 'voice',
+                'fieldName': 'Audio recorder',
+                'inputType': 'audio',
               }
           ],
         },

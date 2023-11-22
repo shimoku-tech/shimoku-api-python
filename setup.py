74b3136f434df6f0bca14c03e84edd27172e9b6d
edgar <edgar.sarria@shimoku.com>
2023-11-22 14:24:33 +0100

    Updated pyscaffold

--- setup.py
+++ setup.py
@@ -1,48 +1,21 @@
-# -*- coding: utf-8 -*-
 """
-    Setup file for shimoku_api_python.
+    Setup file for shimoku-api-python.
     Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 3.2.3.
+    This file was generated with PyScaffold 4.5.
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
-import os
-import sys
-
-from pkg_resources import VersionConflict, require
 from setuptools import setup
 
-
-def auto_versioning(version):
-    import os
-    import re
-
-    github_ref = os.environ.get("GITHUB_REF", "")
-
-
-    if github_ref.startswith("refs/tags/"):
-        version_number = github_ref.split("refs/tags/")[1]
-        version_number = re.sub(r'\.dev\d+', '', version_number)
-        return version_number
-    if github_ref.startswith("refs/heads/release/"):
-        version_number = github_ref.split("refs/heads/release/")[1]
-        version_number = re.sub(r'\.dev\d+', '', version_number)
-        return version_number
-    return version
-
-
-try:
-    require('setuptools>=38.3')
-except VersionConflict:
-    print("Error: version of setuptools is too old (<38.3)!")
-    sys.exit(1)
-
-
 if __name__ == "__main__":
-    requirements = open("requirements.txt").readlines()
-    setup(
-        install_requires=requirements,
-        version=auto_versioning("0.0.0"),
-    )
-
+    try:
+        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+    except:  # noqa
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
